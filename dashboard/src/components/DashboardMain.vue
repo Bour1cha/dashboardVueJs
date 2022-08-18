652ea3423ccabd15e1d8048d313a903b83204bd5
Bour1cha <wassim.bauricha@qrcode-protect.com>
2022-08-18 17:01:11 +0200

    main start

--- dashboard/src/components/DashboardMain.vue
+++ dashboard/src/components/DashboardMain.vue
@@ -1,67 +1,89 @@
-
-
-
 <style lang="scss">
- @import "@/assets/scss/style.scss"; 
+@import "@/assets/scss/style.scss"; 
 </style>
 
 
 <template>
- <body>
+<body>
     <div class= "container">
-      <aside>
-        <div class="top">
-          <div class="logo">
-            <img src="@/assets/img/logo.png" alt="logo">
-            <h2>DIGI<span class="danger">SERVICES</span></h2>
+      <!-- Sidebar -->
+      <x-sidebar/>
+      <main>
+          <h1>Dashboard</h1>
+          <div class="date">
+              <input type="date">
           </div>
-          <div class="close" id="close-btn">
-         <span><font-awesome-icon icon="fa-solid fa-xmark" /></span>   
+
+          <div class="insights">
+            <div class="sales">
+              <span><font-awesome-icon icon="fa-solid fa-euro-sign" /></span>
+                <div class="middle">
+                    <div class="left">
+                        <h3>Ventes Totales</h3>
+                        <h1>25,003€</h1>
+                    </div>
+                    <div class="progress">
+                      <svg>
+                        <circle cx='38' cy='38' r='36'></circle>
+                      </svg>
+                      <div class="number">
+                          <p>81%</p>
+                      </div>
+                    </div>
+                </div>
+                <small class="text-muted">Dernières 24 heures</small>
             </div>
-        </div>
-        <div class="sidebar">
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-box" /></span>   
-            <h3>Dashboard</h3>
-          </a>
-          <a href="#" class="active">
-            <span><font-awesome-icon icon="fa-solid fa-user" /></span>   
-            <h3>Clients</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-clipboard-list" /></span>   
-            <h3>Commandes</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-chart-line" /></span>   
-            <h3>Statistiques</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-message" /></span>   
-            <h3>Messages</h3> <span class="message-count">26</span>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-clipboard" /></span>   
-            <h3>Produits</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-circle-exclamation" /></span>   
-            <h3>Parametres</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-plus" /></span>   
-            <h3>Ajouter un produit</h3>
-          </a>
-          <a href="#">
-            <span><font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" /></span>   
-            <h3>Déconnexion</h3>
-          </a>
-        </div>
-      </aside>
+            <div class="sales">
+              <span><font-awesome-icon icon="fa-solid fa-chart-simple" /></span>
+                <div class="middle">
+                    <div class="left">
+                        <h3>Frais Totales</h3>
+                        <h1>14,203€</h1>
+                    </div>
+                    <div class="progress">
+                      <svg>
+                        <circle cx='38' cy='38' r='36'></circle>
+                      </svg>
+                      <div class="number">
+                          <p>81%</p>
+                      </div>
+                    </div>
+                </div>
+                <small class="text-muted">Dernières 24 heures</small>
+            </div>
+            <div class="sales">
+              <span><font-awesome-icon icon="fa-solid fa-arrow-trend-up" /></span>
+                <div class="middle">
+                    <div class="left">
+                        <h3>Revenu Totales</h3>
+                        <h1>10,603€</h1>
+                    </div>
+                    <div class="progress">
+                      <svg>
+                        <circle cx='38' cy='38' r='36'></circle>
+                      </svg>
+                      <div class="number">
+                          <p>81%</p>
+                      </div>
+                    </div>
+                </div>
+                <small class="text-muted">Dernières 24 heures</small>
+            </div>
+          </div>
+      </main>
     </div>
- </body>
+</body>
 </template>
 
+
+<script>
+import XSidebar from './Sidebar.vue';
+
+export default {
+
+    components: { XSidebar }
+}
+</script>
 
 
 
--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -8,9 +8,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faXmark, faBox , faUser, faClipboardList, faChartLine , faMessage, faClipboard, faCircleExclamation, faPlus, faArrowRightFromBracket} from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faBox , faUser, faClipboardList, faChartLine , faMessage, faClipboard, faCircleExclamation, faPlus, faArrowRightFromBracket,faChartSimple, faArrowTrendUp, faEuroSign} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faXmark, faBox, faUser, faClipboardList, faChartLine, faMessage, faClipboard, faCircleExclamation, faPlus, faArrowRightFromBracket)
+library.add(faXmark, faBox, faUser, faClipboardList, faChartLine, faMessage, faClipboard, faCircleExclamation, faPlus, faArrowRightFromBracket, faChartSimple, faArrowTrendUp, faEuroSign)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
