0d347394c3391bd11a14dc99dfc6077be1fa62f0
Bour1cha <wassim.bauricha@qrcode-protect.com>
2022-09-02 14:54:56 +0200

    message

--- dashboard/src/components/Message/dashmessage.vue
+++ dashboard/src/components/Message/dashmessage.vue
@@ -28,10 +28,39 @@
             </div>
             <div class="rightside">
               <div class="headermessage">
-                
+                <div class="imgText">
+                  <div class="userimg">
+                    <img src="@/assets/img/profile-2.jpg" class="covermessage" alt="profile-pic">
+                  </div>
+                  <h4>André Sanchez <br><span>En ligne</span></h4>
+                </div>
+                <ul class="nav_icons">
+                  <li><font-awesome-icon icon="fa-solid fa-circle-notch" /></li>
+                  <li><font-awesome-icon icon="fa-solid fa-ellipsis-vertical" /></li>
+                </ul>
               </div>
-
+              <div class="chatBox">
+                <div class="message my_message">
+                  <p>Bonjour Fournisseur 1  , rendez-vous la semaine prochaine<br><span>12:31 </span></p>
+                </div>
+                <div class="message frnd_message">
+                  <p>Bonjour , ok pas de soucis à quelle heure ? <br><span>12:31</span></p>
+                </div>
+                <div class="message my_message">
+                  <p>15h ce serait possible pour vous ?<br><span>12:31 </span></p>
+                </div>
+                <div class="message frnd_message">
+                  <p>Oui parfait ! <br><span>12:32</span></p>
+                </div>
+                <div class="message my_message">
+                  <p>d'accord je vous attendrais<br><span>12:33 </span></p>
+                </div>
+                <div class="message frnd_message">
+                  <p>Ok ! <br><span>12:35</span></p>
+                </div>
+              </div>
             </div>
+       
           </div>
 
 
